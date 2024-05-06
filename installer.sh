74051f967891b4ad61663ea391cca78a61b05dbb
farhad-apps <149595369+farhad-apps@users.noreply.github.com>
2024-05-06 22:34:45 +0330

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -41,37 +41,84 @@ get_configs(){
     fi
 }
 
+replace_strings() {
+  local script="$1"
+  local search="$2" 
+  local replace="$3"
+
+  sed "s|$search|$replace|g" <<< "$script"
+}
+
+setup_ssh(){
+    ssh_port=$(get_configs "servers_ssh.port")
+    udp_port=$(get_configs "servers_ssh.udp_port")
+    api_token=$(get_configs "api_token")
+    api_url=$(get_configs "api_url")
+
+    if [ -n "$ssh_port" ]; then
+        ssh_file_url="https://raw.githubusercontent.com/farhad-apps/rc-files/main/setup-ssh.sh"
+        ssh_script=$(curl -s $ssh_file_url)
+
+        ssh_script=$(replace_strings "$ssh_script" "{apiToken}" "$api_token")
+        ssh_script=$(replace_strings "$ssh_script" "{apiUrl}" "$api_url")  
+        ssh_script=$(replace_strings "$ssh_script" "{sshPort}" "$ssh_port")
+        ssh_script=$(replace_strings "$ssh_script" "{udpPort}" "$udp_port")
+
+        bash -c "$ssh_script" 
+    fi
+}
+
+setup_openvpn(){
+    ovpn_port=$(get_configs "servers_openvpn" "port")
+    ovpn_domain=$(get_configs "servers_openvpn" "domain")
+    api_token=$(get_configs "api_token")
+    api_url=$(get_configs "api_url")
+
+    if [ -n "$ovpn_port" ]; then
+        ovpn_file_url="https://raw.githubusercontent.com/farhad-apps/rc-files/main/setup-openvpn.sh"
+        ovpn_script=$(curl -s $ovpn_file_url)
+
+        ovpn_script=$(replace_strings "$ovpn_script" "{api_token}" "$api_token")
+        ovpn_script=$(replace_strings "$ovpn_script" "{api_url}" "$api_url")  
+        ovpn_script=$(replace_strings "$ovpn_script" "{ovpn_port}" "$ovpn_port")
+        ovpn_script=$(replace_strings "$ovpn_script" "{ovpn_domain}" "$ovpn_domain")
+
+        bash -c "$ovpn_script" 
+    fi
+}
+
+setup_v2ray(){
+    vless_tcp_port=$(get_configs "servers_v2ray" "vless_tcp_port")
+    vmess_tcp_port=$(get_configs "servers_v2ray" "vmess_tcp_port")
+
+    if [ -n "$vless_tcp_port" ]; then
+        ovpn_file_url="https://raw.githubusercontent.com/farhad-apps/rc-files/main/setup-v2ray.sh"
+        ovpn_script=$(curl -s $ovpn_file_url)
+        bash -c "$ovpn_script" 
+    fi
+}
+
 # Perform actions based on the parameter
 case $action in
     "default-setup")
         bash <(curl -Ls https://raw.githubusercontent.com/farhad-apps/rc-files/main/install.sh --ipv4)
         ;;
     "setup-ssh")
-        ssh_port=$(get_configs "servers_ssh.port")
-        udp_port=$(get_configs "servers_ssh.udp_port")
-        api_token=$(get_configs "api_token")
-        api_url=$(get_configs "api_url")
-
-        ssh_file_url="https://raw.githubusercontent.com/farhad-apps/rc-files/main/setup-ssh.sh"
-        ssh_file_path="/tmp/rssh"
-        curl -s -o "$ssh_file_path" "$ssh_file_url"
-
-        if [ $? -eq 0 ]; then
-            sed -i "s|{apiToken}|'$api_token'|g" "$ssh_file_path"
-            sed -i "s|{apiUrl}|'$api_url'|g" "$ssh_file_path"
-            sed -i "s|{sshPort}|$ssh_port|g" "$ssh_file_path"
-            sed -i "s|{udpPort}|$udp_port|g" "$ssh_file_path"
-            bash /tmp/rssh > /var/rocket-ssh/install-ssh.log 2>&1 &
-        fi
+        setup_ssh
 
         ;;
     "setup-openvpn")
-        ovpn_port=$(get_configs "servers_openvpn" "port")
-        
+        setup_openvpn
 
         ;;
     "setup-v2ray")
-        ovpn_port=$(get_configs "servers_openvpn" "port")
+        setup_v2ray
+
+        ;;
+    "setup-all")
+        setup_ssh
+        setup_openvpn
+        setup_v2ray
         
         ;;
     *)
