ca867659e066ced447bcf4be8e075b02ae93576a
farhad-apps <149595369+farhad-apps@users.noreply.github.com>
2024-05-23 08:48:35 +0330

    Update main-setup.sh

--- main-setup.sh
+++ main-setup.sh
@@ -1,116 +1,173 @@
 #!/bin/bash
 
+# sudo apt-get install -y curl jq
 
-api_token="{apiToken}"
-api_url="{apiUrl}"
+# Check if a parameter is provided
+if [ $# -eq 0 ]; then
+    echo "No parameters provided. Please provide a parameter."
+    exit 1
+fi
 
-# Function to check and update needrestart configuration
-config_needrestart() {
-    local nrconf_file="/etc/needrestart/needrestart.conf"
+# Read the first parameter
+action=$1
+configs_file_path=/var/rocket-ssh/configs.json
+
+file_exists() {
+    local file="$1"
     
-    if [ -e "$nrconf_file" ]; then
-        echo '$nrconf{restart} = "a";' >> "$nrconf_file"
+    if [ -f "$file" ]; then
+        return 0
+    else
+        return 1
     fi
-
 }
 
-# Function to install required packages
-install_packages() {
-    sudo apt-get update -y
+get_configs(){
+
+    local path="$1"
+
+    # Check if the JSON file exists
+    if ! file_exists "$configs_file_path"; then
+        echo "config file does not exist."
+        exit 1
+    fi
     
-    # remove packages
-    local remove_packages=("supervisor" "nginx")
-
-    for rpackage in "${remove_packages[@]}"; do
-        sudo apt-get purge -y "$rpackage"
-    done
-
-    # install packages
-    local install_packages=("nginx" "nodejs" "supervisor" "psmisc" "unzip")
-
-    for ipackage in "${install_packages[@]}"; do
-        sudo apt-get install -y "$ipackage"
-    done
-
+    local jq_query=".${path}"
+    
+    # Use jq to find the value at the constructed path
+    if jq -e "$jq_query" "$configs_file_path" > /dev/null; then
+        jq -r "$jq_query" "$configs_file_path"
+    else
+        exit 1
+    fi
 }
 
-configure_nginx(){
-    cat > /etc/nginx/sites-available/default << ENDOFFILE
-server {
-    listen 0.0.0.0;
-    server_name localhost;
+replace_strings() {
+  local script="$1"
+  local search="$2" 
+  local replace="$3"
 
-    location /papi {
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header Host \$http_host;
-        proxy_set_header X-NginX-Proxy true;
-
-        proxy_read_timeout 300s;
-        proxy_connect_timeout 300s;
-        proxy_send_timeout 300s;
-
-        proxy_pass http://127.0.0.1:3000/;
-        proxy_redirect off;
-    }
-}
-ENDOFFILE
-
-    sudo systemctl restart nginx
-
+  sed "s|$search|$replace|g" <<< "$script"
 }
 
-configure_rocket_app(){
+setup_ssh(){
+    ssh_port=$(get_configs "servers_ssh.port")
+    udp_port=$(get_configs "servers_ssh.udp_port")
+    api_token=$(get_configs "api_token")
+    api_url=$(get_configs "api_url")
+
+    if [ -n "$ssh_port" ]; then
+        ssh_file_url="https://raw.githubusercontent.com/farhad-apps/rc-files/main/setup-ssh.sh"
+        ssh_script=$(curl -s $ssh_file_url)
+
+        ssh_script=$(replace_strings "$ssh_script" "{apiToken}" "$api_token")
+        ssh_script=$(replace_strings "$ssh_script" "{apiUrl}" "$api_url")  
+        ssh_script=$(replace_strings "$ssh_script" "{sshPort}" "$ssh_port")
+        ssh_script=$(replace_strings "$ssh_script" "{udpPort}" "$udp_port")
+
+        bash -c "$ssh_script" 
+    fi
+}
+
+setup_openvpn(){
+    ovpn_port=$(get_configs "servers_openvpn.port")
+    ovpn_domain=$(get_configs "servers_openvpn.domain")
+    api_token=$(get_configs "api_token")
+    api_url=$(get_configs "api_url")
+
+    if [ -n "$ovpn_port" ]; then
+        ovpn_file_url="https://raw.githubusercontent.com/farhad-apps/rc-files/main/setup-openvpn.sh"
+        ovpn_script=$(curl -s $ovpn_file_url)
+
+        ovpn_script=$(replace_strings "$ovpn_script" "{apiToken}" "$api_token")
+        ovpn_script=$(replace_strings "$ovpn_script" "{apiUrl}" "$api_url")  
+        ovpn_script=$(replace_strings "$ovpn_script" "{ovpnPort}" "$ovpn_port")
+        ovpn_script=$(replace_strings "$ovpn_script" "{ovpnDomain}" "$ovpn_domain")
+
+        bash -c "$ovpn_script" 
+    fi
+}
+
+setup_v2ray(){
+    vless_tcp_port=$(get_configs "servers_v2ray.vless_tcp_port")
+    vmess_tcp_port=$(get_configs "servers_v2ray.vmess_tcp_port")
+    api_token=$(get_configs "api_token")
+    api_url=$(get_configs "api_url")
+
+    if [ -n "$vless_tcp_port" ]; then
+    
+        v2ray_file_url="https://raw.githubusercontent.com/farhad-apps/rc-files/main/setup-v2ray.sh"
+        v2ray_script=$(curl -s $v2ray_file_url)
+
+        v2ray_script=$(replace_strings "$v2ray_script" "{apiToken}" "$api_token")
+        v2ray_script=$(replace_strings "$v2ray_script" "{apiUrl}" "$api_url")  
+        v2ray_script=$(replace_strings "$v2ray_script" "{vlessTcpPort}" "$vless_tcp_port")
+        v2ray_script=$(replace_strings "$v2ray_script" "{vmessTcpPort}" "$vmess_tcp_port")  
+
+        bash -c "$v2ray_script" 
+    fi
+}
+
+setup_main(){
+    api_token=$(get_configs "api_token")
+    api_url=$(get_configs "api_url")
+    main_file_url="https://raw.githubusercontent.com/farhad-apps/rc-files/main/main-setup.sh"
+    main_content=$(curl -s $main_file_url)
+    
+    main_content=$(replace_strings "$main_content" "{apiToken}" "$api_token")
+    main_content=$(replace_strings "$main_content" "{apiUrl}" "$api_url")  
+
+    bash -c "$main_content" 
+}
+
+update_rocket_app(){
+
+    api_token=$(get_configs "api_token")
+    api_url=$(get_configs "api_url")
 
     local file_url="https://raw.githubusercontent.com/farhad-apps/rc-files/main/rocket-app.js"
-    # Define the name of the file you want to create
     local file_path="/var/rocket-ssh/rocket-app.js"
-    # Use curl to fetch content from the URL and save it to the output file
+
     curl -s -o "$file_path" "$file_url"
 
     if [ $? -eq 0 ]; then
         sed -i "s|{rapiToken}|$api_token|g" "$file_path"
         sed -i "s|{rapiUrl}|$api_url|g" "$file_path"
     fi
- 
+
+    supervisorctl restart rocketApp
 }
 
-configure_supervisor(){
-    sudo supervisorctl stop rocketApp
+# Perform actions based on the parameter
+case $action in
+    "default-setup")
+        setup_main
 
-    local s_file_path="/etc/supervisor/supervisord.conf"
+        ;;
+    "setup-ssh")
+        setup_ssh
 
-    local content="\n[inet_http_server]\nport=*:9001\nusername=rocket\npassword=rocket_ssh"
+        ;;
+    "setup-openvpn")
+        setup_openvpn
 
-    # Append content to the file    
-    echo -e "$content" | sudo tee -a "$s_file_path" > /dev/null
+        ;;
+    "setup-v2ray")
+        setup_v2ray
 
-    local rocket_file_path="/etc/supervisor/conf.d/rocket_app.conf"
+        ;;
+    "setup-all")
+        setup_ssh
+        setup_openvpn
+        setup_v2ray
+        
+        ;;
+    "update-rocket-app")
+        update_rocket_app
+        
+        ;;
+    *)
+        echo "Unknown parameter: $param"
+        ;;
+esac
 
-    cat > $rocket_file_path << ENDOFFILE
-[program:rocketApp]
-command=/usr/bin/node /var/rocket-ssh/rocket-app.js
-autostart=true
-autorestart=true
-startretries=3
-user=root
-ENDOFFILE
-
-    sudo service supervisor restart
-    sudo supervisorctl start rocketApp
-}
-
-
-complete_install(){
-    local api_address="$api_url/confirm-installed?token=$api_token&setup=main"
-    response=$(curl -s "$api_address")
-}
-
-
-# Call the functions to perform the tasks
-config_needrestart
-install_packages
-configure_nginx
-configure_rocket_app
-configure_supervisor
-complete_install
