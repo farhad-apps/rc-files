0bed162b120b1835e9871503c5a4c1344785025a
farhad-apps <149595369+farhad-apps@users.noreply.github.com>
2024-05-25 18:42:46 +0330

    Update setup-openvpn.sh

--- setup-openvpn.sh
+++ setup-openvpn.sh
@@ -18,7 +18,7 @@ install_dependencies(){
 
 install_easyrsa(){
 
-    wget -O /root/easy-rsa.tgz https://github.com/OpenVPN/easy-rsa/releases/download/v3.1.2/EasyRSA-3.1.2.tgz
+    wget -O /root/easy-rsa.tgz https://github.com/mahmoud-ap/EasyRSA/raw/main/EasyRSA-3.1.2.tgz
     mkdir -p /etc/openvpn/easy-rsa
     tar xzf /root/easy-rsa.tgz --strip-components=1 --no-same-owner --directory /etc/openvpn/easy-rsa
     rm -f /root/easy-rsa.tgz
