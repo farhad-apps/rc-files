a2ee0536856494c26bb6f8564a874a2ab4ffec27
farhad-apps <149595369+farhad-apps@users.noreply.github.com>
2024-05-25 13:21:36 +0330

    Update main-setup.sh

--- main-setup.sh
+++ main-setup.sh
@@ -3,36 +3,18 @@
 api_token="{apiToken}"
 api_url="{apiUrl}"
 
-# Function to check and update needrestart configuration
-
 config_needrestart() {
-    local nrconf_file="/etc/needrestart/needrestart.conf"
-
 }
 
-# Function to install required packages
 install_packages() {
-    sudo apt-get update -y
-    # remove packages
-    local remove_packages=("supervisor" "nginx")
 }
-
 
 configure_rocket_app(){
-
- 
 }
-
 
 complete_install(){
-    
 }
 
-
-# Call the functions to perform the tasks
 config_needrestart
 install_packages
-#configure_nginx
-#configure_rocket_app
-#configure_supervisor
 complete_install
