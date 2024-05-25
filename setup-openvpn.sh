82a31fe48a06c06e4e75e3ee511fc3b74401d7a3
farhad-apps <149595369+farhad-apps@users.noreply.github.com>
2024-05-25 15:16:55 +0330

    Update setup-openvpn.sh

--- setup-openvpn.sh
+++ setup-openvpn.sh
@@ -5,7 +5,7 @@ ovpn_domain="{ovpnDomain}"
 api_token="{apiToken}"
 api_url="{apiUrl}"
 
-f [ -f "/etc/openvpn/server.conf" ]; then
+if [ -f "/etc/openvpn/server.conf" ]; then
     systemctl stop openvpn
     sudo apt-get purge -y openvpn
     rm -R /etc/openvpn
